d15f8570d1bee03dddb099a896c6958ad2ca92e3
0E-hub <u3u0000@knou.ac.kr>
2024-12-21 15:52:40 +0900

    Initial commit

---
+++ src/main/java/com/young_scheduler/young_scheduler/YoungSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.young_scheduler.young_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/young_scheduler/young_scheduler/YoungSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young_scheduler.young_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
